b055d9b0662bbd5b9e28c94b82572f53e184ab93
Fernando S <89961465+daddyzada@users.noreply.github.com>
2021-09-08 19:44:29 -0300

    Create VoiceChannel.js

---
+++ src/structures/VoiceChannel.js
@@ -0,0 +1,15 @@
+const Base = require('./Base');
+
+module.exports = class VoiceChannel extends Base {
+   constructor(data) {
+     super(data.id)
+
+     this.type = data.type;
+     this.name = data.name;
+     this.nfsw = data.nfsw;
+     this.guildId = data.guild_id;
+     this.bitrate = data.bitrate;
+     this.position = data.position;
+     this.parentId = data.parent_id; 
+  }
+}
