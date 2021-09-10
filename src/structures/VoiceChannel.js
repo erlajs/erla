436d62ac0f1ca541383a6b60b30b2d3001b10dd7
Fernando S <89961465+daddyzada@users.noreply.github.com>
2021-09-10 14:23:53 -0300

    fix: fixing things

--- src/structures/VoiceChannel.js
+++ src/structures/VoiceChannel.js
@@ -1,14 +1,14 @@
-const Base = require('./Base');
+const Base = require('./Base')
 
 module.exports = class VoiceChannel extends Base {
-  constructor(data) {
+  constructor (data) {
     super(data.id)
 
-     this.type = data.type
-     this.name = data.name
-     this.guildId = data.guild_id
-     this.bitrate = data.bitrate
-     this.position = data.position
-     this.parentId = data.parent_id
+    this.type = data.type
+    this.name = data.name
+    this.guildId = data.guild_id
+    this.bitrate = data.bitrate
+    this.position = data.position
+    this.parentId = data.parent_id
   }
 }
