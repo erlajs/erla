c0fd73ca952685ed8053efbbc69a55e496c6e9db
Atsushi <88151256+atshushi@users.noreply.github.com>
2021-09-12 12:33:26 -0300

    Create Webhook.js

---
+++ src/structures/Webhook.js
@@ -0,0 +1,13 @@
+const Base = require('./Base')
+
+module.exports = class DMChannel extends Base {
+  constructor (data) {
+    super(data.id)
+
+    this.username = data.username;
+    this.avatar = data.avatar;
+    this.guildId = data.guild_id;
+    this.channelId = data.channel_id;
+    
+  }
+}
